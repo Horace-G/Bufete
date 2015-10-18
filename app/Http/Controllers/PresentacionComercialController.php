7378015901038238bba8ec113f9294fe1c9c5cc5
Kevin Barahona <kevinbarahn@gmail.com>
2015-10-18 13:52:18 -0600

    bug class

--- app/Http/Controllers/PresentacionComercialController.php
+++ app/Http/Controllers/PresentacionComercialController.php
@@ -1,7 +1,7 @@
 <?php namespace App\Http\Controllers;
 	
 	use App\Http\Controllers\Controller;
-	use App\Http\Requests
+	use App\Http\Requests;
 	
 	class PresentacionComercialController extends Controller {
 		
