04cff89876bdfa74df0c5606f346ad92f32062ab
11241069 <carlos.pagoaga@unitec.edu>
2015-10-20 17:28:30 -0600

    Mensaje

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,4 +1,17 @@
-angular.module('myApp').controller('mainController', ['$scope', '$log', function($scope, $log) {
+angular.module('myApp').controller('mainController', ['$scope', '$log','$http','$location', function($scope, $log,$http,$location) {
+    
+    var path = $location.path($location.path());
+
+		//Creating the baseUrl
+		var baseUrl = path.$$protocol + "://" + path.$$host + ":" + path.$$port + '/Bufete/index.php/allMensaje';
+        var request = {
+			method: 'GET',
+			url: baseUrl
+	   };
+	   $http(request).then(function(response){
+		  console.log(response.data);
+		  
+	   });
     
     $scope.name = 'Main';
     
