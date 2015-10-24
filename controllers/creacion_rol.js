3e65bfb33000ab361f5b93bde75009a3d1f46bd4
a11241069 <cfpagoaga@gmail.com>
2015-10-24 01:42:38 +0000

    as

--- controllers/creacion_rol.js
+++ controllers/creacion_rol.js
@@ -4,38 +4,50 @@ angular.module('myApp').controller('Creacion_Rol', ['$scope','$http','$location'
 		   $scope.symbols = new RegExp("[<>%\$!@#%^&*()_+]");
     
 $scope.submit = function () {
+    
+    $scope.valid = 1;
+    
      if($scope.nombre.length==0){
-         $("#nombreRol").css("background","red");
+         $("#nombreRol").css("color","red");
         toastr.error("Nombre no puede ser vacio","Error(1)");
+        $scope.valid = 0;
          return;
-    }else if($scope.descripcion.length==0){
-        $("#nombreRol").css("background","aliceblue");
-        $("#descripcionRol").css("background","red");
+    }if($scope.descripcion.length==0){
+       // $("#nombreRol").css("background","aliceblue");
+        $("#descripcionRol").css("color","red");
         toastr.error("Descripcion no puede ser vacio","error(1)");
+        $scope.valid = 0;
         return;    
-    }else if($scope.nombre.length>64){
-        $("#nombreRol").css("background","red");
-        $("#descripcionRol").css("background","aliceblue");
+    }if($scope.nombre.length>64){
+        $("#nombreRol").css("color","red");
+       // $("#descripcionRol").css("background","aliceblue");
         toastr.error("ha exedido del tamaño maximo","Error(1)");
+        $scope.valid = 0;
         return;
-    }else if($scope.descripcion.length>128){
-        $("#nombreRol").css("background","aliceblue");
-        $("#descripcionRol").css("background","red");
+    }if($scope.descripcion.length>128){
+        //$("#nombreRol").css("background","aliceblue");
+        $("#descripcionRol").css("color","red");
         toastr.error("ha exedido del tamaño maximo","Error(1)");
+        $scope.valid = 0;
         return;    
-    }else if($scope.symbols.test($scope.nombre)){
-        $("#nombreRol").css("background","red");
-        $("#descripcionRol").css("background","aliceblue");
+    }if($scope.symbols.test($scope.nombre)){
+        $("#nombreRol").css("color","red");
+       // $("#descripcionRol").css("background","aliceblue");
         toastr.error("(2) se han ingresado caracteres no validos en el nombre");
+        $scope.valid = 0;
         return;
-    }else if($scope.symbols.test($scope.descripcion)){
-        $("#nombreRol").css("background","aliceblue");
-        $("#descripcionRol").css("background","red");
+    }if($scope.symbols.test($scope.descripcion)){
+       // $("#nombreRol").css("background","aliceblue");
+        $("#descripcionRol").css("color","red");
         toastr.error("(2) se han ingresado caracteres no validos en la Descripcion");
+        $scope.valid = 0;
         return;
-    }else{
-        $("#nombreRol").css("background","aliceblue");
-        $("#descripcionRol").css("background","aliceblue");
+    }if($scope.symbols.test($scope.nombre) && $scope.symbols.test($scope.descripcion)){
+        
+        
+    }if($scope.valid == 1){
+        $("#nombreRol").css("color","black");
+        $("#descripcionRol").css("color","black");
         var path = $location.path($location.path());
 
 		//Creating the baseUrl
@@ -49,6 +61,8 @@ var request = {
 	$http(request).then(function(response){
 		console.log(response.data);
 		toastr.success("Se Agrego el rol correctamente");
+		$("#nombreRol").css("color","black");
+        $("#descripcionRol").css("color","black");
 		$scope.nombre = '';
 $scope.descripcion = '';
 	});
