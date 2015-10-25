3624b57ce3992249594db39a52ebbfc7fb884bf8
Server <Server@server.com>
2015-10-25 11:09:07 -0600

    migrations

--- app/Http/Controllers/MedicamentoController.php
+++ app/Http/Controllers/MedicamentoController.php
@@ -11,10 +11,21 @@ use DB;
 class MedicamentoController extends Controller
 {
     public function saveMedicamento(Request $request){
-	$nombreM = $request->input('nombreRol');
-        $descR = $request->input('descriptionRol');
-        $estadoR = $request->input('estadoRol');
+	$nombreM = $request->input('nombreMedicamento');
+        $modalidadVentaM = $request->input('modalidadVentaMedicamento');
+	$formaFarmaceuticaM = $request->input('formaFarmaceuticaMedicamento');
+	$presentacionComercialM = $request->input('presentacionComercialMedicamento');
+	$viaAdministracionM = $request->input('viaAdministracionMedicamento');
+	$laboratorioM = $request->input('laboratorioMedicamento');
+        $representanteM = $request->input('representanteMedicamento');
+	$user_created = $request->input('userMedicamento');
+	$estadoM = $request->input('estadoMedicamento');
         $date = Carbon::now();
+
+	DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('medicamento')->insert(
+		array('nombre'=>$nombreM, 'modalidadVentaId'=>$modalidadVentaM, 'formaFarmaceuticaId'=>$formaFarmaceuticaM, 'presentacionComercialId'=>$presentacionComercialM, 'viaAdministracionId'=>$viaAdministracionM, 'laboratorioId'=>$laboratorioM, 'representante'=>$representanteM, 'estado'=>$estadoM, 'user_created'=>$user_created, 'user_updated'=>$user_created, 'created_at'=>$date, 'updated_at'=>$date));
+	return Response::json(array('Success' => 'true'));
     }
 
     public function allMedicamento(){
