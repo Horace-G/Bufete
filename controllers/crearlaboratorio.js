340ef5db802e8b79e14054daaef5fcf5edf5dd6c
11241069 <carlos.pagoaga@unitec.edu>
2015-10-25 09:35:53 -0600

    Laboratorio y Roles

--- controllers/crearlaboratorio.js
+++ controllers/crearlaboratorio.js
@@ -1,10 +1,48 @@
 angular.module('myApp').controller('crear_laboratorio', ['$scope','$http','$location','mensajeService', function ($scope,$http,$location,mensajeService) {
 
- 
+
 $scope.nombre = '';
 $scope.descripcion = '';
-$scope.symbols = new RegExp("[<>%\$!@#%^&*()_+]");    
-$scope.submit = function () {
+$scope.correo = '';
+$scope.symbols = new RegExp("[<>%\$!@#%^&*()_+]");
+$scope.numbers = new RegExp();
+    $('select').material_select();
+$scope.submitNumero = function () {
+   
+    //if($scope.symbols.test($scope.telefonos)){
+    if($('#dropdownid').find(":selected").text()=="Numeros Telefonicos"){
+    var $selectDropdown = 
+      $("#dropdownid")
+       .empty()
+       .html(' ');
+        value = 1;
+    }else{
+        var $selectDropdown = 
+      $("#dropdownid")
+       
+    }
+    // add new value
+    var value = $scope.telefono;
+    $selectDropdown.append(
+      $("<option selected></option>")
+        .attr("number",value)
+        .text(value)
+    );
+
+    // trigger event
+    $selectDropdown.trigger('contentChanged');
+    //}else{
+      //  toastr.error('ERROR!','WUT');     
+    //}
+  };
+
+
+  $('select').on('contentChanged', function() {
+    // re-initialize (update)
+    $(this).material_select();
+  });
+
+$scope.submitGuardar = function () {
     
     $scope.valid = 1;
     
@@ -12,19 +50,19 @@ $scope.submit = function () {
          $("#nombreLaboratorio").css("color","red");
         mensajeService.ShowMessage('INPUT_EMPTY','Nombre Laboratorio');
         $scope.valid = 0;
-         return;
+        
     }if($scope.descripcion.length==0){
        // $("#nombreLaboratorio").css("background","aliceblue");
-        $("#descripcionLaboratorio").css("color","red");
+        $("#direccionLaboratorio").css("color","red");
         mensajeService.ShowMessage('INPUT_EMPTY','Descripcion Laboratorio');
         $scope.valid = 0;
-        return;       
+              
     }if($scope.correo.length==0){
        // $("#nombreLaboratorio").css("background","aliceblue");
-        $("#correoLaboratorio").css("color","red");
+        $("#correoElectronico").css("color","red");
         mensajeService.ShowMessage('INPUT_EMPTY','Correo Laboratorio');
         $scope.valid = 0;
-        return;       
+        
     }if($scope.nombre.length>128){
         $("#nombreLaboratorio").css("color","red");
         //$("#descripcionLaboratorio").css("background","aliceblue");
@@ -33,28 +71,28 @@ $scope.submit = function () {
         return;
     }if($scope.descripcion.length>256){
         //$("#nombreLaboratorio").css("background","aliceblue");
-        $("#descripcionLaboratorio").css("color","red");
+        $("#direccionLaboratorio").css("color","red");
         mensajeService.ShowMessage('LONG_256','Descripcion Laboratorio');
         $scope.valid = 0;
-        return;    
+    
     }if($scope.correo.length>64){
         //$("#correoLaboratorio").css("background","aliceblue");
-        $("#correoLaboratorio").css("color","red");
+        $("#correoElectronico").css("color","red");
         mensajeService.ShowMessage('LONG_64','Correo Laboratorio');
         $scope.valid = 0;
-        return;    
+    
     }if($scope.symbols.test($scope.nombre)){
         $("#nombreLaboratorio").css("color","red");
        // $("#descripcionPresentacionComercial").css("background","aliceblue");
         mensajeService.ShowMessage('INVALID_CHAR','Nombre Laboratorio');
         $scope.valid = 0;
-        return;
+    
     }if($scope.symbols.test($scope.descripcion)){
         //$("#nombrePresentacionComercial").css("background","aliceblue");
-        $("#descripcionlaboratorio").css("color","red");
+        $("#direccionLaboratorio").css("color","red");
         mensajeService.ShowMessage('INVALID_CHAR','Descripcion Laboratorio');
         $scope.valid = 0;
-        return;
+     
     }if($scope.symbols.test($scope.nombre) && $scope.symbols.test($scope.descripcion)){
     
         
@@ -62,19 +100,22 @@ $scope.submit = function () {
         $("#nombreLaboratorio").css("color","black");
         $("#descripcionLaboratorio").css("color","black");
         var path = $location.path($location.path());
-
+        //
+        var NumerosTelefonicos = document.getElementById('dropdownid').options;
+        
 		//Creating the baseUrl
 		var baseUrl = path.$$protocol + "://" + path.$$host + ":" + path.$$port + '/Bufete/index.php/saveLaboratorio';
 		console.log(baseUrl);
 	var request = {
 			method: 'POST',
 			url: baseUrl,
-			data: {nameLaboratorio: $scope.nombre, descriptionLaboratorio: $scope.descripcion,correoLaboratorio: $scope.correo, telefonoLaboratorio: $scope.telefono, userLaboratorio: '1234', estadoLaboratorio: '1'}
+			data: {nameLaboratorio: $scope.nombre, descriptionLaboratorio: $scope.descripcion,correoLaboratorio: $scope.correo, telefonoLaboratorio: NumerosTelefonicos, userLaboratorio: '1234', estadoLaboratorio: '1'}
 	};
+    toastr.error('ERROR!','Guardado');
 	$http(request).then(function(response){
         if(response.data.success.equals("true")){
             console.log(response.data.success);
-            mensajeService.ShowMessage('SUCCESS_SAVE','Laboratorio');
+            
              $("#nombreLaboratorio").css("color","black");
             $("#descripcionLaboratorio").css("color","black");
             $scope.nombre = '';
