95b2720482b0d3ac226e3053a1714fee849b7d21
11241069 <carlos.pagoaga@unitec.edu>
2015-10-25 21:03:40 -0600

    gg

--- controllers/CrearUsuario.js
+++ controllers/CrearUsuario.js
@@ -5,7 +5,7 @@ $scope.init = function () {
        roles=data;
     
     var count = Object.keys(roles).length;
-    alert(count);
+    alert(roles[0]);
     for(var i=0;i<count;i++){
         
         var $selectDropdown = 
