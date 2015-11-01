f49fe678cc84ec34909cffda255bc738c1dc77d3
Kevin Barahona <kevinbarahn@gmail.com>
2015-11-01 10:59:31 -0600

    login test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -80,5 +80,6 @@ Route::get('/allPermiso','PermisoController@allPermiso');
 
 Route::get('/allRolPermiso','RolPermisoController@allRolPermiso');
 
-
-Route::post('/autenticacion','AutenticacionController@authenticate');
+Route::get('/auth/login', 'Auth\AuthController@getLogin');
+Route::post('/auth/login', 'Auth\AuthController@postLogin');
+Route::get('/auth/logout', 'Auth\AuthController@getLogout');
--- app/Usuario.php
+++ app/Usuario.php
@@ -3,9 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
-class Usuario extends Model
+class Usuario extends Model implements AuthenticatableContract,
+                                    AuthorizableContract
 {
+                                        
+    use Authenticatable, Authorizable
     protected $table= "usuario";
     protected $fillable = ['id', 'username', 'password','nombre','estado','rol_id'];
 }
