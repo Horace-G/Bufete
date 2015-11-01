acd50cf79a010d700539e3fc95006dc3f4c14660
Kevin Barahona <kevinbarahn@gmail.com>
2015-11-01 10:39:06 -0600

    no login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
     */
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
-    protected $redirectPath = '/home';
+    protected $redirectPath = '/';
     protected $loginPath = '/login';
     /**
      * Create a new authentication controller instance.
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,9 +18,9 @@
 }]);
 */
 
-Route::get('/', ['middleware' => 'auth', function () {
+Route::get('/',function () {
     return view('index');
-}]);
+});
 
 Route::get('/login',function(){
     return view('login');
