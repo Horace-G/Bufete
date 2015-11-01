048c4072e46cd31fb55e86dd6323ec4ae3d98e74
11241069 <Server@server.com>
2015-11-01 14:31:50 -0600

    redirect logout

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -9,13 +9,14 @@ angular.module('myApp').controller('mainController', ['$scope', '$log','$http','
     $scope.logout = function(){
         var path = $location.path($location.path());
         var baseUrl = path.$$protocol + "://" + path.$$host + ":" + path.$$port +'/Bufete/logout';
-        var request = {
+        var redirectUrl = path.$$protocol+"://"+path.$$host+":"+path.$$port+"/Bufete";
+	var request = {
                 method: 'POST',
                 url: baseUrl,
                 data: {}
         };	
         $http(request).then(function(response){
-            
+		window.location.replace(redirectUrl);	           
         });
     };
     ctrl.init();
