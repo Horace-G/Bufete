e11e09a727b570a9e79e98c616b0851815abc1fc
Kevin Barahona <kevinbarahn@gmail.com>
2015-11-02 21:20:51 -0600

    message

--- config/app.php
+++ config/app.php
@@ -144,6 +144,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Grimthorr\LaravelToast\ServiceProvider::class,
 
     ],
 
@@ -193,6 +194,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+          'Toast' => Grimthorr\LaravelToast\Facade:class,
 
     ],
 
