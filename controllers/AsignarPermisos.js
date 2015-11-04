d085b4ff921285ef9ce7a620fddfc9219b5c6a8b
11241069 <carlos.pagoaga@unitec.edu>
2015-11-03 18:05:29 -0600

    gg

--- controllers/AsignarPermisos.js
+++ controllers/AsignarPermisos.js
@@ -58,10 +58,7 @@ angular.module('myApp').controller('AsignarPermisos', ['$scope','$http','$locati
     };
     
     $scope.submit = function () {
-        if($scope.selectedOption.hasOwnProperty("id")){
-            alert("true");
-            return;
-        }
+        console.log($scope.selectedOption);
         var SelectedPermisos=[];
         var sizetable=$('#table tr').length-1;
         for(var i=1;i<=sizetable;i++){
