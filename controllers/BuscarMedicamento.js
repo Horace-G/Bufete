b886794a040f2c63b166c931deb721aa40305134
11241069 <carlos.pagoaga@unitec.edu>
2015-11-08 22:01:44 -0600

    gg

--- controllers/BuscarMedicamento.js
+++ controllers/BuscarMedicamento.js
@@ -36,20 +36,11 @@ angular.module('myApp').controller('buscar_medicamento', ['$scope','$http','$loc
         if(typeof(Storage) !== "undefined") {
 
         sessionStorage.IdMedicamento=item.MedicamentoId;
-        //window.location=document.getElementById('ver').href;
+        window.location=document.getElementById('ver').href;
 
+            
+      
 
-            sessionStorage.IdMedicamento=item.MedicamentoId;
-            //alert(item.MedicamentoId);
-            //$("a").attr("href", "#/verExpediente");
-<<<<<<< HEAD
-            $location.path('#/verExpediente');
-            //window.location=document.getElementById('ver').href;
-
-=======
-            //$location.path('#/verExpediente');
-            window.location=document.getElementById('ver').href;
->>>>>>> d3b133766ba82b6fc58a73c4c52db02ec269ca8c
         }else{
             
         }
