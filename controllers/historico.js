6f93eb721c60e036df2da57cd080c8259a0063c9
11241069 <carlos.pagoaga@unitec.edu>
2015-11-08 22:26:21 -0600

    gg

--- controllers/historico.js
+++ controllers/historico.js
@@ -13,24 +13,15 @@ angular.module('myApp').controller('VerExpediente', ['$scope','$http','$location
         $http(request).then(function(response){
             Medicamentos=response.data;
             console.log(Medicamentos);
-            $scope.nombreFormaFarmaceutica=Medicamentos[0].FormaFarmaceuticaNombre;
-            $scope.nombreViaAdministracion=Medicamentos[0].ViaAdminstracionNombre;
-            $scope.nombreModalidadVenta=Medicamentos[0].ModalidadNombre;
-            $scope.nombrePresentacionComercial=Medicamentos[0].PresentacionComercialNombre;
             $scope.nombre=Medicamentos[0].MedicamentoNombre;
             $scope.codigo=Medicamentos[0].MedicamentoId;
             $scope.estado=Medicamentos[0].CicloVidaNombre;
-            $scope.representante=Medicamentos[0].NombreRepresentante;
-             if(typeof(Storage) !== "undefined") {
-                sessionStorage.IdMedicamento=$scope.codigo;
-                }else{   
-                }
+            $scope.laboratorio=Medicamentos[0].LaboratorioNombre;
+            
         });
     };
     ctrl.init();
-    $scope.historico = function(){
-      window.location=document.getElementById('historico').href;  
-    };
+    
 
     
 }]);
