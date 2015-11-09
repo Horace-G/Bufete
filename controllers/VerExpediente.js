e2ff75933878a27265d46674f0922ae23c004709
11241069 <carlos.pagoaga@unitec.edu>
2015-11-08 21:01:58 -0600

    gg

--- controllers/VerExpediente.js
+++ controllers/VerExpediente.js
@@ -3,7 +3,16 @@ angular.module('myApp').controller('VerExpediente', ['$scope','$http','$location
    
     ctrl.init = function(){
        alert(sessionStorage.IdMedicamento);
-        alert("ggg");
+       var path = $location.path($location.path());
+        var baseUrl = path.$$protocol + "://" + path.$$host + ":" + path.$$port + '/Bufete/index.php/getMedicamento';
+        var request = {
+                method: 'POST',
+                url: baseUrl,
+                data: {idMedicamento: sessionStorage.IdMedicamento}
+        };
+        $http(request).then(function(response){
+            console.log(response.data);
+        });
     };
     ctrl.init();
     
