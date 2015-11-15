09afc179926ef4d2545b0318161e115ba0443e78
Kevin Barahona <kevinbarahn@gmail.com>
2015-11-14 18:03:07 -0600

    test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,11 +22,7 @@
             }
         </style>
         
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/ng-dialog/0.5.5/css/ngDialog-theme-default.min.css"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/ng-dialog/0.5.5/css/ngDialog-theme-plain.min.css"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/ng-dialog/0.5.5/css/ngDialog.min.css"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/ng-dialog/0.5.5/js/ngDialog.min.js"></script>
-        
+              
         
         <!-- load JQuery, angular and materialize JS via CDN -->
         
