7fd0938c14f1427d9c89fea0938df7dc56e14388
Kevin Barahona <kevinbarahn@gmail.com>
2015-11-16 18:10:53 -0600

    asd

---
+++ database/seeds/PermisoUsuarioSeeder.php
@@ -0,0 +1,38 @@
+<?php
+use Illuminate\Database\Seeder;
+ 
+class PermisoTableSeeder extends Seeder {
+ 
+    public function run()
+    {
+        // Uncomment the below to wipe the table clean before populating
+        DB::table('rol_permiso')->delete();
+ 
+        $permisos = array(
+        ['rol_id'=>1,'permiso_id'=>1,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>2,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>3,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>4,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>5,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>6,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>7,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>8,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>9,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>10,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>13,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>14,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>15,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>16,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>17,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>18,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>19,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>20,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>21,'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['rol_id'=>1,'permiso_id'=>22,'created_at' => new DateTime, 'updated_at' => new DateTime],
+        );
+ 
+        // Uncomment the below to run the seeder
+        DB::table('rol_permiso')->insert($permisos);
+    }
+ 
+}
