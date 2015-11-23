6d8a3b5946e14dd464474bf8064b923be6f56960
11241069 <carlos.pagoaga@unitec.edu>
2015-11-23 13:54:51 -0600

    gg

--- controllers/modificarExpediente.js
+++ controllers/modificarExpediente.js
@@ -180,5 +180,113 @@ angular.module('myApp').controller('modificarExpediente', ['$scope','$http','$lo
     });
     ctrl.init();
     
+    $scope.submit = function () {
+    
+    $scope.valid = 1;
+    var id_rol = 0;
+    var id_presentacioncomercial = 0;
+    var id_modalidadventa = 0;
+    var id_viaadministracion = 0;
+    var id_formafarmaceutica = 0;
+    var id_laboratorio = 0;
+    $scope.symbolsExp = new RegExp("[<>%\$!@#%^&*()_+]");
+    
+    if($scope.nombre.length == 0){
+         mensajeService.ShowMessage('INPUT_EMPTY','Nombre');
+        $scope.valid = 0;
+        ngDialog.open({ template: 'templateId' });
+    }
+    
+    if($scope.responsable.length == 0){
+         mensajeService.ShowMessage('INPUT_EMPTY','Responsable');
+        $scope.valid = 0;
+    }
+    
+    if($scope.responsable.length>64){
+         mensajeService.ShowMessage('LONG_64','Responsable');
+        $scope.valid = 0;
+    }
+    
+    if($scope.nombre.length>64){
+         mensajeService.ShowMessage('LONG_64','Nombre');
+        $scope.valid = 0;
+    }
+    
+    if($scope.symbolsExp.test($scope.nombre)){
+        mensajeService.ShowMessage('INVALID_CHAR','Nombre');
+        $scope.valid = 0;
+    }
+    
+    if($scope.symbolsExp.test($scope.responsable)){
+        mensajeService.ShowMessage('INVALID_CHAR','Responsable');
+        $scope.valid = 0;
+    }
+    
+     if($scope.valid == 1){
+        
+        var path = $location.path($location.path());
+        //
+         var count = Object.keys(modalidadventa).length;
+         for(var i=0;i<count;i++){
+            if($('#modalidadVenta').find(":selected").text()==modalidadventa[i].nombre){
+                id_modalidadventa=modalidadventa[i].id;
+                
+                break;
+            }
+        }
+        var count = Object.keys(presentacioncomercial).length;
+         for(var i=0;i<count;i++){
+            if($('#presentacionComercial').find(":selected").text()==presentacioncomercial[i].nombre){
+                id_presentacioncomercial=presentacioncomercial[i].id;
+                
+                break;
+            }
+        }
+         var count = Object.keys(formafarmaceutica).length;
+         for(var i=0;i<count;i++){
+            if($('#formaFarmaceutica').find(":selected").text()==formafarmaceutica[i].nombre){
+                id_formafarmaceutica=formafarmaceutica[i].id;
+                
+                break;
+            }
+        }
+        var count = Object.keys(viaadministracion).length;
+         for(var i=0;i<count;i++){
+            if($('#viaAdministracion').find(":selected").text()==viaadministracion[i].nombre){
+                id_viaadministracion=viaadministracion[i].id;
+                
+                break;
+            }
+        }
+         var count = Object.keys(laboratoriofabricante).length;
+         for(var i=0;i<count;i++){
+            if($('#laboratorioFabricante').find(":selected").text()==laboratoriofabricante[i].nombre){
+                id_laboratorio=laboratoriofabricante[i].id;
+                
+                break;
+            }
+        }
+		//Creating the baseUrl
+		var baseUrl = path.$$protocol + "://" + path.$$host + ":" + path.$$port + '/Bufete/index.php/updateMedicamento';
+		console.log(baseUrl);
+         
+	var request = {
+			method: 'POST',
+			url: baseUrl,
+			data: {nombreMedicamento: $scope.nombre,modalidadVentaMedicamento: id_modalidadventa, formaFarmaceuticaMedicamento: id_formafarmaceutica, viaAdministracionMedicamento: id_viaadministracion, presentacionComercialMedicamento: id_presentacioncomercial,laboratorioMedicamento: id_laboratorio, responsableMedicamento: $scope.responsable, userMedicamento: '1234', estadoMedicamento: '1'}
+	};
+    
+	$http(request).then(function(response){
+        if(response.data.Success=="true"){
+            console.log(response.data.success);
+            mensajeService.ShowMessage('SUCCESS_SAVE','Expediente');
+        }else{
+            mensajeService.ShowMessage('FAILED_SAVE','Expediente');
+        }
+	});
+    }
+
+
+};
     
 }]);
